83a1c7ffb7e470be6209a4db713a996e3bbc05df
Pattrigue <patrick@gamehouse.dk>
2020-11-22 13:27:03 +0100

    Update InvalidEmailException.cs

--- DashSystem/Users/InvalidEmailException.cs
+++ DashSystem/Users/InvalidEmailException.cs
@@ -4,14 +4,6 @@ namespace DashSystem.Users
 {
     public sealed class InvalidEmailException : Exception
     {
-        public string Email { get; }
-        
-        public InvalidEmailException() { }
-
-        public InvalidEmailException(string message) : base(message) { }
-
-        public InvalidEmailException(string message, Exception inner) : base(message, inner) { }
-
         public InvalidEmailException(string message, string email) : base($"Illegal email \"{email}\": {message}") { }
     }
 }
