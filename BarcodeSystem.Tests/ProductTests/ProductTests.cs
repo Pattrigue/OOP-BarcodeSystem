8a25e7ca3184d53313ce1b680ad783ef0b333781
Pattrigue <patrick@gamehouse.dk>
2020-11-23 19:48:15 +0100

    Create user tests and product tests

---
+++ BarcodeSystem.Tests/ProductTests/ProductTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using BarcodeSystem.Products;
+using NUnit.Framework;
+
+namespace BarcodeSystem.Tests.ProductTests
+{
+    public sealed class ProductTests
+    {
+        [TestCase(1U)]
+        [TestCase(50U)]
+        [TestCase(5000U)]
+        [TestCase(uint.MaxValue)]
+        public void SetId_SetTo1OrPositive_ExpectNoException(uint id)
+        { 
+            Product product = CreateProduct();
+
+            Assert.DoesNotThrow(() => product.Id = id);
+        }
+        
+        [Test]
+        public void SetId_SetTo0OrNegative_ExpectArgumentException()
+        { 
+            Product product = CreateProduct();
+
+            Assert.Throws<ArgumentException>(() => product.Id = 0);
+        }
+
+        [Test]
+        public void SetName_SetToNull_ExpectArgumentException()
+        {
+            Product product = CreateProduct(); 
+
+            Assert.Throws<ArgumentException>(() => product.Name = null);
+        }
+
+        private static Product CreateProduct()
+        {
+            return new Product(1, "Test", 50m, true);
+        }
+    }
+}
---
+++ BarcodeSystem.Tests/UserTests/UserTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using BarcodeSystem.Users;
+using NUnit.Framework;
+
+namespace BarcodeSystem.Tests.UserTests
+{
+    public sealed class UserTests
+    {
+        [TestCase(null)]
+        [TestCase("email")]
+        [TestCase("email@example")]
+        [TestCase("email@-example.com")]
+        [TestCase("email@example.com_")]
+        [TestCase("email@example.com_")]
+        [TestCase("email@example.com_")]
+        [TestCase("email@.example.com")]
+        [TestCase("email@example.com.")]
+        public void SetEmail_SetToIncorrectFormat_ExpectAssertion(string email)
+        {
+            User user = CreateUser();
+
+            Assert.Throws<InvalidEmailException>(() => user.Email = email);
+        }
+
+        [TestCase("FirstName", "LastName", null)]
+        [TestCase("FirstName", null, "username")]
+        [TestCase(null, "LastName", "username")]
+        public void SetFirstNameLastNameUserName_SetToNull_ExpectAssertion(string firstName, string lastName, string username)
+        {
+            User user = CreateUser();
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                user.FirstName = firstName;
+                user.LastName = lastName;
+                user.Username = username;
+            });
+        }
+
+        private static User CreateUser()
+        {
+            return new User(10, "First", "Last", "username", "email@example.com", 50m);
+        }
+    }
+}
