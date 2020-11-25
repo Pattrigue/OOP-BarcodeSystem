ecc359e00ea3e79dff6fedebae292866a6e006ae
Pattrigue <patrick@gamehouse.dk>
2020-11-25 18:56:32 +0100

    Update BarcodeSystemManagerTests.cs

--- BarcodeSystem.Tests/BarcodeSystemManagerTests/BarcodeSystemManagerTests.cs
+++ BarcodeSystem.Tests/BarcodeSystemManagerTests/BarcodeSystemManagerTests.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Transactions;
 using BarcodeSystem.Core;
 using BarcodeSystem.Products;
+using BarcodeSystem.Transactions;
 using BarcodeSystem.Users;
+using NSubstitute.Extensions;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 
 namespace BarcodeSystem.Tests.BarcodeSystemManagerTests
 {
@@ -16,8 +20,8 @@ namespace BarcodeSystem.Tests.BarcodeSystemManagerTests
         [TestCase(null, 0)]
         public void GetUsers_GetByUsername_CountIsExpectedValue(int id, int expectedCount)
         {
-            BarcodeSystemManager sys = new BarcodeSystemManager();
-            IEnumerable<IUser> users = sys.GetUsers((user => user.Id == id));
+            BarcodeSystemManager systemManager = new BarcodeSystemManager();
+            IEnumerable<IUser> users = systemManager.GetUsers((user => user.Id == id));
 
             Assert.AreEqual(expectedCount, users.Count());
         }
@@ -25,17 +29,90 @@ namespace BarcodeSystem.Tests.BarcodeSystemManagerTests
         [Test]
         public void GetProductById_GetFirstProduct_ExpectDoesNotThrow()
         {
-            BarcodeSystemManager sys = new BarcodeSystemManager();
+            BarcodeSystemManager systemManager = new BarcodeSystemManager();
             
-            Assert.DoesNotThrow(() => sys.GetProductById(1));
+            Assert.DoesNotThrow(() => systemManager.GetProductById(1));
         }
         
         [Test]
         public void GetProductById_GetInvalidProduct_ExpectAssertion()
         {
-            BarcodeSystemManager sys = new BarcodeSystemManager();
+            BarcodeSystemManager systemManager = new BarcodeSystemManager();
             
-            Assert.Throws<ProductNotFoundException>(() => sys.GetProductById(0));
+            Assert.Throws<ProductNotFoundException>(() => systemManager.GetProductById(0));
+        }
+        
+        [Test]
+        public void GetActiveProducts_GetAll_ExpectIsActive()
+        {
+            BarcodeSystemManager systemManager = new BarcodeSystemManager();
+            IEnumerable<IProduct> activeProducts = systemManager.ActiveProducts;
+
+            foreach (IProduct product in activeProducts)
+            {
+                Assert.IsTrue(product.IsActive);
+            }
+        }
+        
+        [Test]
+        public void UserBalanceWarning_UserLowBalance_ExpectEventInvoked()
+        {
+            BarcodeSystemManager systemManager = new BarcodeSystemManager();
+            IUser user = systemManager.GetUsers((u => u.Username != null)).First();
+            
+            bool wasEventInvoked = false;
+
+            systemManager.UserBalanceWarning += (user, balance) => wasEventInvoked = true; 
+            user.Balance = 5;
+            
+            Assert.IsTrue(wasEventInvoked);
+        }
+        
+        [Test]
+        public void AddCreditsToAccount_BalanceChange_ExpectBalanceUpdated()
+        {
+            BarcodeSystemManager systemManager = new BarcodeSystemManager();
+            IUser user = systemManager.GetUsers((u => u.Username != null)).First();
+
+            decimal amountToAdd = 50;
+            decimal initialBalance = user.Balance;
+            
+            systemManager.AddCreditsToAccount(user, amountToAdd);
+            
+            Assert.AreEqual(initialBalance + amountToAdd, user.Balance);
+        }
+        
+        [Test]
+        public void BuyProduct_UserBuysProduct_ExpectInsufficientCreditsException()
+        {
+            BarcodeSystemManager systemManager = new BarcodeSystemManager();
+            IUser user = CreateUser(0);
+            IProduct product = CreateProduct(100, false);
+
+            Assert.Throws<InsufficientCreditsException>(() => systemManager.BuyProduct(user, product));
+        }
+        
+        [TestCase(50, 50, false)]
+        [TestCase(100, 50, false)]
+        [TestCase(25, 50, true)]
+        public void BuyProduct_UserBuysProduct_ExpectDoesNotThrowInsufficientCreditsException
+            (decimal userBalance, decimal productPrice, bool canBeBoughtOnCredit)
+        {
+            BarcodeSystemManager systemManager = new BarcodeSystemManager();
+            IUser user = CreateUser(userBalance);
+            IProduct product = CreateProduct(productPrice, canBeBoughtOnCredit);
+
+            Assert.DoesNotThrow(() => systemManager.BuyProduct(user, product));
+        }
+        
+        private static User CreateUser(decimal balance)
+        {
+            return new User(1, "First", "Last", "username", "email@example.com", balance);
+        }
+
+        private static IProduct CreateProduct(decimal price, bool canBeBoughtOnCredit)
+        {
+            return new Product(1, "Product", 5m, true, canBeBoughtOnCredit);
         }
     }
 }
